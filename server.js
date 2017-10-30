322b956bee17a15eda8add1be76595a368be0f4b
Mitch Burgin <mitchellburgin@outlook.com>
2017-10-30 21:56:29 +1000

    add a get-handler to express server

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 var express = require('express')
 var app = express()
 
+app.get('/', function (req, res) {
+  res.send('Yay node girls!')
+})
+
 app.listen(8080, function () {
 
-  console.log('Started listening server on port 3000')
+  console.log('Started listening server on port 3000. Ready to accept connections.')
 })
